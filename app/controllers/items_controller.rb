6a2868fc2cfdcabd438afb880787457edb1ba30c
Alexey <kuznetsov@mystand.ru>
2015-09-17 00:35:49 +0600

    simple json uploader withou any ajax

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,15 @@
 class ItemsController < ApplicationController
+  def index
+    @item = Item.new
+    @items = Item.all
+  end
+
+
+  def create
+    @item  = Item.new
+    file = params[:item][:data]
+    @item.data = JSON.parse(File.read(file.tempfile))
+    @item.save!
+    redirect_to items_path unless request.xhr?
+  end
 end
